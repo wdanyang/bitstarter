44885b21529134624cbf214926fac4760419010d
Delia Wu <delia.w.cn@gmail.com>
2013-07-23 02:43:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
- var express = require('express');
+var express = require('express');
 
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', 
 function(request, response) {
-  response.send(fs.readFileSync('./index.html', 'utf8'));
+  var html=fs.readFileSync(htmlfile).toString();
+  response.send(html);
 }
 );
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
